062c9a6bf15f6f180f8ab3cf09aea04ec56fa666
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-02-07 03:14:55 -0500

    add card and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,56 @@
+import { CardCountry } from '@/components/shared';
+import { Button } from '@/components/ui/button';
+import { ChevronDown } from 'lucide-react';
+
+const optionsCard = [
+	{
+		id: 1,
+		countryIcon: '/img/colombia.png',
+		moneyAvailable: true,
+		description: 'PESO COLOMBIANO',
+		acronym: 'COP'
+	},
+	{
+		id: 2,
+		countryIcon: '/img/united-states.png',
+		moneyAvailable: false,
+		description: 'US DOLLAR',
+		acronym: 'Pronto'
+	},
+	{
+		id: 3,
+		countryIcon: '/img/european-union.png',
+		moneyAvailable: false,
+		description: 'EURO',
+		acronym: 'Pronto'
+	}
+];
+
 export default function Home() {
-	return <div>Pagina Inicial</div>;
+	return (
+		<div className="flex flex-col gap-5">
+			<div>
+				<h6 className="text-xs mb-2">BALANCE TOTAL</h6>
+				<span className="text-3xl text-muted-foreground">$</span>
+				<span className="font-[500] text-3xl">10.120.000,</span>
+				<span className="font-[500] text-[16px]">75</span>
+				<span className="text-muted-foreground text-[20px]"> COP</span>
+			</div>
+
+			<div>
+				<Button className="mr-2 w-200 h-10 rounded-xl">
+					Enviar dinero <ChevronDown />
+				</Button>
+				<Button className="w-200 h-10 rounded-xl" variant="outline">
+					Recargar dinero
+				</Button>
+			</div>
+
+			<div className="flex flex-row gap-5">
+				{optionsCard.map((i) => (
+					<CardCountry key={i.id} i={i} />
+				))}
+			</div>
+		</div>
+	);
 }
---
+++ src/components/shared/CardCountry.tsx
@@ -0,0 +1,48 @@
+import { Avatar, AvatarImage } from '../ui/avatar';
+
+interface Props {
+	i: {
+		countryIcon: string;
+		moneyAvailable: boolean;
+		description: string;
+		acronym: string;
+	};
+}
+
+export const CardCountry = ({
+	i: { description, countryIcon, moneyAvailable, acronym }
+}: Props) => {
+	return (
+		<div className="bg-[#F6F6F6] w-[200px] h-[200px] rounded-xl p-4 flex flex-col">
+			<div className="flex flex-1 justify-between">
+				<Avatar className="h-16 w-16 border-[3px] border-[#D9D9D9]">
+					<AvatarImage src={countryIcon} />
+				</Avatar>
+
+				{moneyAvailable ? (
+					<span className="text-muted-foreground text-[12px] tracking-[.05em]">
+						{acronym}
+					</span>
+				) : (
+					<span className="text-[10px] tracking-[.05em]">
+						<div className="bg-[#D9D9D9] rounded-xl p-1">Pronto</div>
+					</span>
+				)}
+			</div>
+
+			{moneyAvailable && (
+				<div>
+					<span className="text-muted-foreground">$</span>
+					<span className="font-[500]">10.120.000,</span>
+					<span className="font-[500] text-[12px]">75</span>
+				</div>
+			)}
+
+			<div>
+				<span className="text-muted-foreground text-[10px] tracking-[.05em]">
+					{description}
+				</span>
+			</div>
+		</div>
+	);
+};
